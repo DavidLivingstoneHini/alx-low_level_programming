9f7c6eff27e533d7c8890d6d2752ca4b00897072
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-06-24 16:17:16 +0000

    Test for positive or negative integer

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function for positive or negative integer
 * Return: 0
 */
 
@@ -9,8 +9,8 @@ int main(void)
 {
         int i;
 
-        i = 98;
-        positive_or_negative(i);
+        i = 0;
 
+        positive_or_negative(i);
         return (0);
 }
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,21 @@
+# C Debugging.
+
+This directory contains files that debug a particular program
+
+- At ALX we learn by taking on real projects, we are given projects to complete each day and push to Github, these projects are automatically reviewed each day with a score checking tool.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
