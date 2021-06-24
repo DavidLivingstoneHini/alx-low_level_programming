8555dd5629e42e13eb7a4b025a266288b3b3ca8b
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-06-24 19:14:23 +0000

    Header File

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+
 void positive_or_negative(int);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
